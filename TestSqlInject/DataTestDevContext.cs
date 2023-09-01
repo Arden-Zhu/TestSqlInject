28a8fd10c2f3d9890a3c5fd626e798fdfe2d861d
Arden Zhu <arden.zhu@lafayette148.com>
2023-09-01 11:54:09 -0400

    Add project files.

---
+++ TestSqlInject/DataTestDevContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSqlInject
+{
+    internal class DataTestDevContext : DbContext
+    {
+        public DbSet<Test> Tests { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "server=148.148.148.97;database=datatestdev;trusted_connection=true;TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public List<Test> sp148Test(string a)
+        {
+            var tests = this.Tests
+                .FromSql($"EXECUTE sp148Test {a}")
+                .ToList();
+            return tests;
+        }
+    }
+
+    [Table("test")]
+    [Keyless]
+    public class Test
+    {
+        public string a { get; set; }
+    }
+}
---
+++ TestSqlInject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestSqlInject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTestDevContext db = new DataTestDevContext();
+            var tests = db.sp148Test("a'b'c");
+            Console.WriteLine(tests[tests.Count - 1]);
+        }
+    }
+}
